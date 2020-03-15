e20aae2c33f728645dc582a242706cbadd261218
sunagaga <rijeka.kogegmail.com>
2020-03-15 01:13:28 +0100

    feat: tutorial part2

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Startup Name Generator',
+      // default theme is dependent on the device
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
       home: RandomWords(),
     );
   }
@@ -17,16 +21,56 @@ class MyApp extends StatelessWidget {
 class RandomWordsState extends State<RandomWords> {
   // any identifier that starts with an underscore(_)
   // means private to its library.
-  final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18.0);
+  // Dart language doesn't have Public/Protected/Private statement
+  final List<WordPair> _suggestions = <WordPair>[];
+  final Set<WordPair> _saved = Set<WordPair>();
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Startup Name Generator'),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+        ],
       ),
       body: _buildSuggestions(),
+    );
+  }
+
+  void _pushSaved() {
+    // push the route to the Navigator's stack
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          final Iterable<ListTile> tiles = _saved.map(
+                (WordPair pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          // ListTile.divideTiles() adds horizontal spacing between each ListTile
+          // The "divided" variable holds the final rows, converted by toList()
+          final List<Widget> divided = ListTile
+              .divideTiles(
+            context: context,
+            tiles: tiles,
+          )
+              .toList();
+
+          return Scaffold(
+              appBar: AppBar(
+                title: Text('Saved Suggestions'),
+              ),
+              body: ListView(children: divided)
+          );
+        },
+      ),
     );
   }
 
@@ -52,11 +96,30 @@ class RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair);
     return ListTile(
-        title: Text(
-          pair.asPascalCase,
-          style: _biggerFont,
-        )
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing: Icon(
+        alreadySaved? Icons.favorite: Icons.favorite_border,
+        color: alreadySaved? Colors.redAccent: null,
+      ),
+      onTap: () {
+        setState(() {
+          // calling setState() triggers a call to build() method for the State object
+          // resulting in an update to the UI
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
+      onLongPress: () {
+        // TODO: want to do something here later
+      },
     );
   }
 }
